6db484bdbf2ffd7571072c90386cedd1cd4abe0c
kusi <appaukusi05@gmail.com>
2025-04-16 12:24:43 +0000

    hardcoded the game page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import Notice from "./components/Notice";
+import { languages } from "./languages";
 
 function App() {
-  
-
+  const languagesList = languages.map((language) => {
+    return (
+      <div
+        style={{
+          backgroundColor: language.backgroundColor,
+          color: language.color,
+        }}
+      >
+        {language.name}
+      </div>
+    );
+  });
   return (
     <>
-      
+      <Header />
+      <Notice />
+      <section className="elimination">
+        <div className="languages-container">{languagesList}</div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Notice.jsx
@@ -0,0 +1,10 @@
+export default function Notice() {
+    return (
+        <>
+        <section className="notice">
+            <h2>You Win!</h2>
+            <p>Well done! 🎉</p>
+        </section>
+        </>
+    )
+}
