4fa2c2fce89d5a2e9b2df0c20dd8dfc1e21bf327
kusi <appaukusi05@gmail.com>
2025-04-16 13:27:52 +0000

    finished creating the static ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Notice from "./components/Notice";
 import { languages } from "./languages";
 
 function App() {
+  const [currentWord, setCurrentWord] = useState("react");
+
+  const word = currentWord.split("").map((letter, index) => {
+    return <span key={index}>{letter.toUpperCase()}</span>;
+  });
+  console.log(word);
   const languagesList = languages.map((language) => {
     return (
       <div
@@ -16,12 +23,26 @@ function App() {
       </div>
     );
   });
+
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+  const keys = alphabet
+    .split("")
+    .map((key) => <button key={key}>{key.toUpperCase()}</button>);
+
   return (
     <>
-      <Header />
-      <Notice />
+      <section className="top">
+        <Header />
+        <Notice />
+      </section>
       <section className="elimination">
         <div className="languages-container">{languagesList}</div>
+      </section>
+      <section className="choosen-word">{word}</section>
+      <section className="keyboard">{keys}</section>
+      <section className="new-game">
+        <button>New Game</button>
       </section>
     </>
   );
