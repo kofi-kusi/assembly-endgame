aa30857afb0d291a58ce32167bfcad4a958a9e2d
kusi <appaukusi05@gmail.com>
2025-04-17 10:40:04 +0000

    feat: change in background of keys for guessing letter

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,28 @@ import "./App.css";
 import Header from "./components/Header";
 import Notice from "./components/Notice";
 import { languages } from "./languages";
+import { clsx } from "clsx"
 
 function App() {
   const [currentWord, setCurrentWord] = useState("react");
+  const [guessedLetters, setGuessedLetters] = useState([])
+
+  console.log(guessedLetters)
+
+  function addGuessedLetter(key){
+    setGuessedLetters(prev => {
+      return prev.includes(key) ? prev : [...prev, key]
+    })
+  }
 
   const word = currentWord.split("").map((letter, index) => {
     return <span key={index}>{letter.toUpperCase()}</span>;
   });
-  console.log(word);
+
   const languagesList = languages.map((language) => {
     return (
       <div
+        key={language.name}
         style={{
           backgroundColor: language.backgroundColor,
           color: language.color,
@@ -28,7 +39,24 @@ function App() {
 
   const keys = alphabet
     .split("")
-    .map((key) => <button key={key}>{key.toUpperCase()}</button>);
+    .map((key) => {
+      const isGuessed = guessedLetters.includes(key);
+      const isCorrect = isGuessed && currentWord.includes(key);
+      const isWrong = isGuessed && !currentWord.includes(key);
+      const className = clsx({
+        correct: isCorrect,
+        wrong: isWrong
+      })
+
+      return (
+      <button 
+        className={className}
+        key={key} 
+        onClick={() => addGuessedLetter(key)}>
+          {key.toUpperCase()}
+      </button>
+    )
+    });
 
   return (
     <>
